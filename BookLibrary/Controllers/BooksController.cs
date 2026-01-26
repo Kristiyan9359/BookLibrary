158832fcad68b2894eea9632439c9c990bdc83a3
Kristiyan9359 <kristiyankamboshev@gmail.com>
2026-01-26 13:20:18 +0200

    Implement book create functionality with validation

---
+++ BookLibrary/Controllers/BooksController.cs
@@ -0,0 +1,79 @@
+﻿namespace BookLibrary.Controllers;
+
+using BookLibrary.Data;
+using BookLibrary.Data.Models;
+using BookLibrary.ViewModels.Books;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+public class BooksController : Controller
+{
+    private readonly ApplicationDbContext context;
+
+    public BooksController(ApplicationDbContext context)
+    {
+        this.context = context;
+    }
+
+    public IActionResult Create()
+    {
+        var model = new BookCreateViewModel
+        {
+            Authors = context.Authors
+                .Select(a => new SelectListItem
+                {
+                    Value = a.Id.ToString(),
+                    Text = a.FirstName + " " + a.LastName
+                })
+                .ToList(),
+
+            Genres = context.Genres
+                .Select(g => new SelectListItem
+                {
+                    Value = g.Id.ToString(),
+                    Text = g.Name
+                })
+                .ToList()
+        };
+
+        return View(model);
+    }
+
+    [HttpPost]
+    public IActionResult Create(BookCreateViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            model.Authors = context.Authors
+                .Select(a => new SelectListItem
+                {
+                    Value = a.Id.ToString(),
+                    Text = a.FirstName + " " + a.LastName
+                })
+                .ToList();
+
+            model.Genres = context.Genres
+                .Select(g => new SelectListItem
+                {
+                    Value = g.Id.ToString(),
+                    Text = g.Name
+                })
+                .ToList();
+
+            return View(model);
+        }
+
+        var book = new Book
+        {
+            Title = model.Title,
+            Pages = model.Pages,
+            AuthorId = model.AuthorId,
+            GenreId = model.GenreId
+        };
+
+        context.Books.Add(book);
+        context.SaveChanges();
+
+        return RedirectToAction(nameof(Create));
+    }
+}
---
+++ BookLibrary/ViewModels/Books/BookCreateViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace BookLibrary.ViewModels.Books;
+
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+
+public class BookCreateViewModel
+{
+    [Required]
+    [MaxLength(150)]
+    public string Title { get; set; } = null!;
+
+    [Range(1, 5000)]
+    public int Pages { get; set; }
+
+    [Required]
+    [Display(Name = "Author")]
+    public int AuthorId { get; set; }
+
+    [Required]
+    [Display(Name = "Genre")]
+    public int GenreId { get; set; }
+
+    public IEnumerable<SelectListItem> Authors { get; set; }
+        = new List<SelectListItem>();
+
+    public IEnumerable<SelectListItem> Genres { get; set; }
+        = new List<SelectListItem>();
+}
