d8a53070e47a6a116b9562a865845663c3077595
Kristiyan9359 <kristiyankamboshev@gmail.com>
2026-03-04 12:07:51 +0200

    Add author create and index view models

---
+++ BookLibrary.ViewModels/Authors/AuthorCreateViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace BookLibrary.ViewModels.Authors;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using static BookLibrary.Common.ValidationConstants;
+
+
+public class AuthorCreateViewModel
+{
+    [Required(ErrorMessage = "First name is requiered")]
+    [MinLength(AuthorNameMinLength)]
+    [MaxLength(AuthorNameMaxLength)]
+    public string FirstName { get; set; } = null!;
+
+    [Required(ErrorMessage = "Last name is requiered")]
+    [MinLength(AuthorNameMinLength)]
+    [MaxLength(AuthorNameMaxLength)]
+    public string LastName { get; set; } = null!;
+
+    [Required]
+    public int CountryId { get; set; }
+
+    public IEnumerable<SelectListItem> Countries { get; set; }
+       = new List<SelectListItem>();
+}
---
+++ BookLibrary.ViewModels/Authors/AuthorIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BookLibrary.ViewModels.Authors;
+
+public class AuthorIndexViewModel
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string Country { get; set; } = null!;
+}
