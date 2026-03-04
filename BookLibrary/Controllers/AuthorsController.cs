e320bfec934b4be8fb718d0874bcd2a0aa5d7966
Kristiyan9359 <kristiyankamboshev@gmail.com>
2026-03-04 12:08:55 +0200

    Add author controller and views

---
+++ BookLibrary/Controllers/AuthorsController.cs
@@ -0,0 +1,64 @@
+﻿namespace BookLibrary.Web.Controllers;
+
+using BookLibrary.Services.Core.Contracts;
+using BookLibrary.ViewModels.Authors;
+using Microsoft.AspNetCore.Mvc;
+
+
+public class AuthorsController : Controller
+{
+    private readonly IAuthorService authorService;
+
+    public AuthorsController(IAuthorService authorService)
+    {
+        this.authorService = authorService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        try
+        {
+            var authors = await authorService.GetAllAsync();
+            return View(authors);
+        }
+        catch (Exception)
+        {
+            return RedirectToAction("Error", "Home");
+        }
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Create()
+    {
+        try
+        {
+            AuthorCreateViewModel model = await authorService.GetCreateAsync();
+            return View(model);
+        }
+        catch (Exception)
+        {
+            return RedirectToAction("Error", "Home");
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(AuthorCreateViewModel model)
+    {
+        try
+        {
+            if (!ModelState.IsValid)
+            {
+                model.Countries = (await authorService.GetCreateAsync()).Countries;
+                return View(model);
+            }
+
+            await authorService.CreateAsync(model);
+
+            return RedirectToAction(nameof(Index));
+        }
+        catch (Exception)
+        {
+            return RedirectToAction("Error", "Home");
+        }
+    }
+}
