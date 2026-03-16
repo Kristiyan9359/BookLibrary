699d36fecb01eed1720a38b3f194386cdd3a1794
Kristiyan9359 <kristiyankamboshev@gmail.com>
2026-03-16 12:21:05 +0200

    Add book cover (ImageUrl) field to Create and Edit forms

--- BookLibrary.Data/Seed/DbSeeder.cs
+++ BookLibrary.Data/Seed/DbSeeder.cs
@@ -47,6 +47,7 @@ public static class DbSeeder
                 Title = "Animal Farm",
                 Year = 1945,
                 Pages = 112,
+                ImageUrl = "https://i0.wp.com/paperlanternslit.com/wp-content/uploads/2021/03/AnimalFarm-1.jpg?fit=1535%2C2339&ssl=1",
                 AuthorId = authors[0].Id,
                 GenreId = genres[4].Id,
                 OwnerId = demoUser.Id
@@ -56,6 +57,7 @@ public static class DbSeeder
                 Title = "Harry Potter and the Philosopher’s Stone",
                 Year = 1997,
                 Pages = 223,
+                ImageUrl = "https://res.cloudinary.com/bloomsbury-atlas/image/upload/w_360,c_scale,dpr_1.5/jackets/9781408855652.jpg",
                 AuthorId = authors[1].Id,
                 GenreId = genres[1].Id,
                 OwnerId = demoUser.Id
@@ -65,6 +67,7 @@ public static class DbSeeder
                 Title = "Murder on the Orient Express",
                 Year = 1934,
                 Pages = 256,
+                ImageUrl = "https://m.media-amazon.com/images/I/71ihbKf67RL._AC_UF1000,1000_QL80_.jpg",
                 AuthorId = authors[2].Id,
                 GenreId = genres[3].Id,
                 OwnerId = demoUser.Id
