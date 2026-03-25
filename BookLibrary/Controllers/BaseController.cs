e79b22c50e789ff721361b2b1cb6ca38c7827b39
Kristiyan9359 <kristiyankamboshev@gmail.com>
2026-03-25 13:56:34 +0200

    Add antiforgery token

--- BookLibrary/Controllers/BaseController.cs
+++ BookLibrary/Controllers/BaseController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 
 [Authorize]
+[AutoValidateAntiforgeryToken]
 public class BaseController : Controller
 {
     protected string? GetUserId()
